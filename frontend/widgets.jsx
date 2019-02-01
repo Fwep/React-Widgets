5650b68e167f4fd78c755e8ef17e81a2a597f0a3
Fwep <tarancacacho@gmail.com>
2019-01-31 21:12:58 -0800

    Render Tabs component

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs'
 
 function Root() {
+  const tabs = [
+    {title: 'Taran', content: 'is the man!'},
+    {title: 'React-Redux', content: 'Name a better combo...'},
+    {title: 'Hello', content: '...World (what else?)'},
+  ]
+
   return (
     <div>
+      <h1 id="clock">Clock</h1>
       <Clock />
+      <h1 id="clock">Tabs</h1>
+      <Tabs tabs={tabs}/>
     </div>
   );
 };
