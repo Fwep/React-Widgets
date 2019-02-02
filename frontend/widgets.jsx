bf28b52ad7e69b54e477b139995d6a14f2bfd471
Fwep <tarancacacho@gmail.com>
2019-02-01 20:21:01 -0800

    Render Weather and Autocomplete widget, and add list of names

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
-import Tabs from './tabs'
+import Tabs from './tabs';
+import Weather from './weather';
+import Autocomplete from './autocomplete';
 
 function Root() {
   const tabs = [
@@ -11,12 +13,22 @@ function Root() {
     {title: 'Hello', content: '...World (what else?)'},
   ]
 
+  const names = [
+    "Taran", "Sameeh", "Jon", "Ted", "Mark", "Jerrik", "Joanna", "Jaya","Stroud","Steven", "Micah",
+    "Melvin", "Arriola", "Choi", "Anthony", "Ben", "John", "Safuh", "Ali", "Savio", "Chase", "Filipp",
+    "Evan"
+  ];
+
   return (
     <div>
       <h1 id="clock">Clock</h1>
       <Clock />
       <h1 id="clock">Tabs</h1>
       <Tabs tabs={tabs}/>
+      <h1 id="clock">Weather</h1>
+      <Weather/>
+      <h1 id="clock">Autocomplete</h1>
+      <Autocomplete names={names} />
     </div>
   );
 };
